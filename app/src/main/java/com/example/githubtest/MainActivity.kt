44a63b89cd5eb3180547b033614cc0f897cd67b5
syoukyakuro55 <110616458+syoukyakuro55@users.noreply.github.com>
2022-10-31 14:10:45 +0900

    create serect activity_layout.

--- app/src/main/java/com/example/githubtest/MainActivity.kt
+++ app/src/main/java/com/example/githubtest/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.example.githubtest
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.example.githubtest.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.homeImage.setOnClickListener{onhomeImageTapped(it)}
+    }
+
+    fun onhomeImageTapped(view: View?){
+        val intent = Intent(this, activity_layouts::class.java)
+        startActivity(intent)
     }
 }
