7b657a15bebc69af2028642dfbe111fce588c424
syoukyakuro55 <110616458+syoukyakuro55@users.noreply.github.com>
2022-10-31 14:21:44 +0900

    write kotlin MainActivity and activity_layout.

--- app/src/main/java/com/example/githubtest/activity_layouts.kt
+++ app/src/main/java/com/example/githubtest/activity_layouts.kt
@@ -1,11 +1,36 @@
 package com.example.githubtest
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.example.githubtest.databinding.ActivityLayoutsBinding
 
 class activity_layouts : AppCompatActivity() {
+    private lateinit var binding: ActivityLayoutsBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_layouts)
+        binding = ActivityLayoutsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.movieButton.setOnClickListener { onMovieButtonTapped(it) }
+        binding.movieboardButton.setOnClickListener { onMovieBoardButtonTapped(it) }
+        binding.boardBotton.setOnClickListener { onBoardButtonTapped(it) }
+    }
+
+    fun onMovieButtonTapped(view: View?){
+        val intent = Intent(this, /* 動画画面名 */::class.java)
+        startActivity(intent)
+    }
+
+    fun onMovieBoardButtonTapped(view: View?){
+        val intent = Intent(this, /* 動画+戦術ボード画面名 */::class.java)
+        startActivity(intent)
+    }
+
+    fun onBoardButtonTapped(view: View?){
+        val intent = Intent(this, /* 戦術ボード画面名 */::class.java)
+        startActivity(intent)
     }
 }
