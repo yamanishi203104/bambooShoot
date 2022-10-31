4345af9d5956b2fce883b74a782f6e54e76528a7
AnthonyBonner <203118@ocsjoho.onmicrosoft.com>
2022-10-31 09:53:09 +0900

    動画編集画面（ビデオ再生）作成

---
+++ app/src/main/java/com/example/githubtest/videoEditor.kt
@@ -0,0 +1,17 @@
+package com.example.githubtest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.Spinner
+
+
+class videoEditor : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_video_editor)
+    }
+}
+
