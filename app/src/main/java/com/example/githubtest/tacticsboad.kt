2a62321008de311b590026a88714fb74148d6b3f
AnthonyBonner <203118@ocsjoho.onmicrosoft.com>
2022-11-02 10:08:20 +0900

    動画編集画面（戦術ボード）作成

---
+++ app/src/main/java/com/example/githubtest/tacticsboad.kt
@@ -0,0 +1,11 @@
+package com.example.githubtest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class tacticsboad : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tacticsboad)
+    }
+}
