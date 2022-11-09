be824d0fd9404ea8bb527775055b9ace0b5905ee
yamanishi203104 <kchansyacho@gmail.com>
2022-11-09 11:47:07 +0900

    yokomuki

--- app/src/main/java/com/example/githubtest/EditActivity1.kt
+++ app/src/main/java/com/example/githubtest/EditActivity1.kt
@@ -2,10 +2,43 @@ package com.example.githubtest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.AdapterView
+import android.widget.Spinner
+import com.example.githubtest.databinding.ActivityEdit1Binding
 
 class EditActivity1 : AppCompatActivity() {
+    private lateinit var binding: ActivityEdit1Binding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_edit1)
+        binding = ActivityEdit1Binding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val myView :MyView = findViewById(R.id.myView)
+
+        /*
+        binding.choosePenColorSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                val spinner = parent as? Spinner
+                val item = spinner?.selectedItem as? String
+                item?.let {
+                    if (it.isNotEmpty()){
+                        MyView.changeColor(it)
+                    }
+                }
+            }
+        }*/
+
+        binding.colortest.setOnClickListener {
+            myView.changeColor("黄")
+        }
     }
 }
