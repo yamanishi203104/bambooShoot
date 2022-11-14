b9f3f287a828ce00d7454122b4d4d07c3de90dcb
yamanishi203104 <kchansyacho@gmail.com>
2022-11-14 12:19:57 +0900

    write play video.

--- app/src/main/java/com/example/githubtest/EditActivity1.kt
+++ app/src/main/java/com/example/githubtest/EditActivity1.kt
@@ -1,5 +1,8 @@
 package com.example.githubtest
 
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -7,16 +10,30 @@ import android.view.LayoutInflater
 import android.view.SurfaceView
 import android.view.View
 import android.widget.AdapterView
+import android.widget.Button
 import android.widget.Spinner
+import android.widget.VideoView
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.example.githubtest.databinding.ActivityEdit1Binding
 
 class EditActivity1 : AppCompatActivity() {
     private lateinit var binding: ActivityEdit1Binding
 
+    private val REQUEST_GALLERY_TAKE = 2
+    private val RECORD_REQUEST_CODE = 1000
+
+    private lateinit var storage_vv: VideoView
+    private lateinit var storage_btn: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityEdit1Binding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        storage_vv = binding.videoView
+        storage_btn = binding.readVideoButton
+        setupPermissions()
 
         val customSurfaceView = CustomSurfaceView(this, binding.surfaceView)
         binding.surfaceView.setOnTouchListener { v, event ->
@@ -55,7 +72,43 @@ class EditActivity1 : AppCompatActivity() {
             customSurfaceView.reset()
         }
 
+        storage_btn.setOnClickListener {
+            openGalleryForImage()
+        }
+
         Log.d("★","error")
+    }
+
+    private fun openGalleryForImage(){
+        val intent = Intent(Intent.ACTION_PICK)
+        intent.type = "video/*"
+        startActivityForResult(intent,REQUEST_GALLERY_TAKE)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        when(requestCode){
+            2->{
+                if (resultCode == Activity.RESULT_OK && requestCode == REQUEST_GALLERY_TAKE){
+                    storage_vv.setVideoURI(data?.data)
+                    binding.videoView.start()
+                    Log.d("★","error")
+                }
+            }
+        }
+    }
+
+    private fun setupPermissions(){
+        val permission = ContextCompat.checkSelfPermission(this,android.Manifest.permission.READ_EXTERNAL_STORAGE)
+
+        if(permission != PackageManager.PERMISSION_GRANTED){
+            makeRequest()
+        }
+    }
+
+    private fun makeRequest(){
+        ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE),RECORD_REQUEST_CODE)
     }
 }
 
